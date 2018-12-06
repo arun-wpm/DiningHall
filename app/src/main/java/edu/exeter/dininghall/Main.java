fa6c7ba7a04d06c3470fafef5caa0c8b2e152ac5
RayaBurong25-1 <arun92166@gmail.com>
2018-12-06 09:20:51 -0500

    12/6/18 During Campbell's Class

--- app/src/main/java/edu/exeter/dininghall/Main.java
+++ app/src/main/java/edu/exeter/dininghall/Main.java
@@ -18,13 +18,13 @@ public class Main extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.breakfast);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.lunch);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.dinner);
                     return true;
             }
             return false;
