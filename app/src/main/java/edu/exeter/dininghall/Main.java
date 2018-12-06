8559284f136553325d05c4f90719c2f2f840e0f9
RayaBurong25-1 <arun92166@gmail.com>
2018-12-06 09:30:29 -0500

    12/6/2018 Also During Campbell's Class

--- app/src/main/java/edu/exeter/dininghall/Main.java
+++ app/src/main/java/edu/exeter/dininghall/Main.java
@@ -5,11 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ListView listView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,8 +39,19 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        listView = (ListView) findViewById(R.id.listView);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //TODO: inflate listView
+
+        //TODO: breakfast, lunch and dinner
+
+        //TODO: settings menu
+
+        //TODO: day tabs
+
+        //TODO: rating
     }
 
 }
