a51d4ca78145974ccb77bd119dd33181e4ef7255
RayaBurong25-1 <arun92166@gmail.com>
2018-12-07 23:56:56 -0500

    12/7/2018 started JSON parsing and RecyclerView

--- app/src/main/java/edu/exeter/dininghall/Main.java
+++ app/src/main/java/edu/exeter/dininghall/Main.java
@@ -1,17 +1,31 @@
 package edu.exeter.dininghall;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import org.json.*;
 
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
 public class Main extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private ListView listView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    //http://www.json.org/
+    //http://stleary.github.io/JSON-java/index.html
+    JSONArray MenuArray = new JSONArray(R.string.source);
+    int MealSelected;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,17 +35,24 @@ public class Main extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.breakfast);
+                    MealSelected = 0;
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.lunch);
+                    MealSelected = 1;
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.dinner);
+                    MealSelected = 2;
                     return true;
             }
             return false;
         }
     };
+
+    public Main() throws JSONException {
+        //TODO: error boxes
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +60,34 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        listView = (ListView) findViewById(R.id.listView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        //TODO: inflate listView
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
 
-        //TODO: breakfast, lunch and dinner
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        //Display according to navigation bar / meal selected
+        //https://developer.android.com/guide/topics/ui/layout/recyclerview
+        //TODO: display breakfast, lunch and dinner by inflation
+        switch(MealSelected)
+        {
+            case 0:
+
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
 
         //TODO: settings menu
 
@@ -54,4 +96,54 @@ public class Main extends AppCompatActivity {
         //TODO: rating
     }
 
+}
+
+//I just copied from the Android Developer guide lol
+//TODO: make this work
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        ...
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
